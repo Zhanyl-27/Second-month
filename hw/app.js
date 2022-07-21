3fc414af75e3f84c6a144aa68035f50f60a836d9
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-07-22 00:41:42 +0600

    Add files via upload

---
+++ hw/app.js
@@ -0,0 +1,17 @@
+let numbers = [23, 72, 73, 24, 15];
+let result = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    result = result + numbers[i];
+}
+console.log(result)
+
+
+
+
+// let i = 50; 
+
+// while(i > 0){
+//     console.log(i)
+//     i--;
+// }
