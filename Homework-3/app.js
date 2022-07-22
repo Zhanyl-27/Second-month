b4112157692d5d3715fb4ff3295f723789cf0748
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-07-22 19:34:51 +0600

    Homework-3

---
+++ Homework-3/app.js
@@ -0,0 +1,44 @@
+// let robots = [
+//     {
+//         name: "Fiksik",
+//         power: "250"
+//     },
+//     {
+//         name: "Dobi",
+//         power:"400"
+//     },
+//     {
+//         name: "Robik",
+//         power: "350"
+//     },
+//     {
+//         name: "Shurshik",
+//         power: "370"
+//     },
+//     {
+//         name: "Lelik",
+//         power: "420"
+//     },
+// ]
+
+// const filteredRobots = [];
+
+// for(let user of robots){
+//     if(user.power > 370){
+//         filteredRobots.push(user)
+//     }
+// }
+
+// console.log(filteredRobots)
+
+
+
+
+for(let i = 0; i < 21; i++) {
+    if( i == 5 || i == 10. || i ==15 ) continue;
+    console.log(i)
+}
+
+
+
+
