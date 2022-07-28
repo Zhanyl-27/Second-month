6cd5f2f6368f8ad20412d168430e094eae8a911e
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-07-28 12:09:42 +0600

    Homework-1

---
+++ Homework-1/app.js
@@ -0,0 +1,39 @@
+const name = "Animals"
+console.log(name)
+
+let cow = {
+    name: "Burenka",
+    age: 10,
+    calf: "Mumu"
+  };
+  console.log(cow.name);
+  
+  let cat = {
+      name: "Murka",
+      age: 5,
+      kitty: "Senseya"
+  };
+  console.log(cat.age);
+  
+  let dog = {
+      name: "Reks",
+      age: 7,
+      puppy: "Bruno"
+  };
+  console.log(dog.name);
+
+  
+
+ const first_number = prompt("Первое число?")
+const second_number = prompt("Второе число?")
+
+if(first_number > second_number) {
+  console.log(first_number + " > " + second_number)
+} else if(first_number < second_number) {
+  console.log(first_number + " < " + second_number)
+} else {
+  console.log(first_number + " = " + second_number)
+}
+
+
+
