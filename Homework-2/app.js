57701d6a8376508f7c4ebd690001236906f341a5
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-07-28 12:05:05 +0600

    Add files via upload

---
+++ Homework-2/app.js
@@ -0,0 +1,21 @@
+// function calcSum(numOne, numTwo){
+//     console.log("Переменная numOne:" + numOne);
+//     console.log("Переменная numTwo:" + numTwo);
+
+//     let sum = numOne + numTwo;
+//     console.log("Сумма:" + sum);
+// }
+// calcSum(70, 7);
+// prompt("numOne" , "70")
+// prompt("numTwo" , "7")
+
+
+
+
+
+function getOptions(text , name) {
+    let getSum = text + name
+    return getSum
+}
+
+console.log("Hi", " Geektech")
