72cf5eace2a544bbb323c88b01d5d1502a3341d4
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-08-07 15:22:44 +0600

    Homework-6

---
+++ Homework-6/app.js
@@ -0,0 +1,55 @@
+
+// const button = document.getElementById('button');
+// const input = document.getElementById('input');
+
+// console.log(button)
+
+// button.onclick = () => {
+//     const tag = document.createElement('div');
+
+//     tag.setAttribute('class', 'block');
+//     tag.innerHTML = input.value;
+//     document.body.append(tag);
+    
+// const deleteButton = document.createElement('button');
+// deleteButton.innerText = "Delete"
+
+// deleteButton.onclick = () => {
+//     tag.remove()
+// }
+// tag.append(deleteButton);
+// input.value = ""; Кнопка очищения
+// }
+
+
+
+// ДЗ№2
+
+//     function buttonClick() {
+// 	let elem = document.getElementById('elem');
+//     let subButton = document.getElementById("subButton");
+    
+// 	elem.innerHTML = 'Пока!';
+//     subButton.addEventListener("click", () => {
+//         alert('Changed!')
+//     });
+// }
+
+// ДЗ№2 правильный вариант
+
+// let h2 = document.getElementById("h2");
+// let buttonChange = document.querySelector("#hi");
+
+// buttonChange.onclick = () => {
+//     alert("Changed!")
+//     h2.textContent = "Пока!";
+// }
+
+// let wrap = document.querySelector('.wrap')
+// console.log(wrap)
+// console.log(wrap.innerHTML)
+// h2.innerHTML = `<h5 style="color: red" >Я пятый</h5>`;
+
+
+
+
