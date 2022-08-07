7cc16d6e8013ba4db8e811a77fb400ebfa9da568
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-08-07 23:36:14 +0600

    Homework-7

---
+++ Homework-7/app.js
@@ -0,0 +1,27 @@
+// ДЗ№1 вариант 1
+
+// let change_image = document.getElementById("change_image");
+// let buttonChange = document.querySelector("#img");
+// buttonChange.onclick = () => {
+//     change_image.src = "img/Photo.2.jpg";
+// }
+
+
+
+// ДЗ№1 вариант 2 посмотреть hw.html и StyleSheet.css
+
+
+
+
+// ДЗ№2
+
+// h2.addEventListener("mouseenter", function() {
+//     console.log("Мышь на тексте")
+//     // console.log(this)
+//     this.style.color = "red"
+// })
+
+// h2.addEventListener("mouseleave", function() {
+//     console.log("Мышь покинул текст")
+//     this.style.color = "green"
+// })
